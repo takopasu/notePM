903d6934022b5da724ad8dd87785eb8e93ad0812
takopasu <s22mf11@ge.osaka-sandai.ac.jp>
2022-10-20 16:39:06 +0900

    記事投稿作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,8 +7,9 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600"  rel="stylesheet">
     </head>
-    <body 
+    <body>
         <h1>Blog Name</h1>
+        <a href="/posts/create">create</a>
         <div class='posts'>
             @foreach ($posts as $post)
             <div class='post'>
