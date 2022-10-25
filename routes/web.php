9a7da1bfcc7683002ea78e08d2cea2b61ee6f664
takopasu <s22mf11@ge.osaka-sandai.ac.jp>
2022-10-25 20:25:00 +0900

    Breezse

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,24 @@ use App\Http\Controllers\CategoryController;
 |
 */
 
-Route::get ('/' , [PostController::class,'index']);
-Route::get('/posts/create',[PostController::class,'create']);
-Route::get('/posts/{post}',[PostController::class,'show']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/{post}/edit',[PostController::class,'edit']);
-Route::put('/posts/{post}',[PostController::class,'update']);
-Route::delete('/posts/{post}',[PostController::class,'delete']);
-Route::get('/categories/{category}',[CategoryController::class,'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::controller(PostController::class)->middleware(['auth'])->group(function(){
+    Route::get('/','index')->name('index');
+    Route::post('/posts','store')->name('store');
+    Route::get('/posts/create', 'create')->name('create');
+    Route::get('/posts/{post}', 'show')->name('show');
+    Route::post('/posts/{post}','update')->name('update');
+    Route::delete('/posts/{post}', 'delete')->name('delete');
+    Route::get('/posts/{post}/edit', 'edit')->name('edit');
+    });
+    
+// Route::get('/categories/{category}',[CategoryController::class,'index'])->name('index')->middleware('auth');
